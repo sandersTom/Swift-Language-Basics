be7bd541bdcf83a6d63290ffdf3d269b59725599
t s <ts18@ts-macbook-pro.centurylink.net>
2018-02-02 18:59:27 -0600

    Initial Commit

---
+++ Swift_Language_Basics/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift_Language_Basics
+//
+//  Created by t s on 2/2/18.
+//  Copyright © 2018 TomSanders. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
